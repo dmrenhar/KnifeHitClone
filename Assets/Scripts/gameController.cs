6db6d5973e53a8857d90aba4436749d87447a381
dmrenhar <enhardmr@gmail.com>
2022-06-02 08:53:18 +0300

    Knife Hİt Clone Publish

---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(gameUI))]
+public class gameController : MonoBehaviour
+{
+    public static gameController Instance { get; private set; }
+
+    [SerializeField]
+    private int knifeCount;
+
+    [Header("knife Spawning")]
+    [SerializeField]
+    private Vector2 knifeSpawnPos;
+    [SerializeField]
+    private GameObject knifeObject;
+
+    public gameUI gameUI { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        gameUI = GetComponent<gameUI>();
+    }
+
+    private void Start()
+    {
+        gameUI.SetIntialDisplayedKnideCount(knifeCount);
+        SpawnKnife();
+    }
+
+
+    public void OnSuccessfulKnifeHit()
+    {
+        if (knifeCount>0)
+        {
+            SpawnKnife();
+        }
+        else
+        {
+            StartGameOverSequence(true);
+        }
+    }
+
+    private void SpawnKnife()
+    {
+        knifeCount--;
+        Instantiate(knifeObject, knifeSpawnPos, Quaternion.identity);
+
+    }
+
+    public void StartGameOverSequence(bool win)
+    {
+        StartCoroutine("GameOverSequenceCoroutine", win);
+    }
+
+    private IEnumerator GameOverSequenceCoroutine(bool win)
+    {
+        if (win)
+        {
+            yield return new WaitForSecondsRealtime(0.3f);
+            RestartGame();
+        }
+        else
+        {
+            gameUI.ShowRestartButton();
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/logRotation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logRotation : MonoBehaviour
+{
+    [System.Serializable]
+    private class RotationELement
+    {
+        #pragma warning disable 0649
+        public float speed;
+        public float duration;
+        #pragma warning restore 0649
+    }
+
+    [SerializeField]
+    private RotationELement[] rotationPattern;
+
+    private WheelJoint2D wheelJoint;
+    private JointMotor2D motor;
+
+    private void Awake()
+    {
+        wheelJoint = GetComponent<WheelJoint2D>();
+        motor = new JointMotor2D();
+        StartCoroutine("PlayRotationPattern");
+    }
+
+    private IEnumerator PlayRotationPattern()
+    {
+        int rotationIndex = 0;
+        while(true)
+        {
+            yield return new WaitForFixedUpdate();
+
+            motor.motorSpeed = rotationPattern[rotationIndex].speed;
+            motor.maxMotorTorque = 10000;
+            wheelJoint.motor = motor;
+
+
+            yield return new WaitForSeconds(rotationPattern[rotationIndex].duration);
+            rotationIndex++;
+            rotationIndex = rotationIndex < rotationPattern.Length ? rotationIndex: 0;
+        }
+    }
+}
